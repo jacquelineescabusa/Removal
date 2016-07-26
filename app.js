81e30f877030696e0fc74094d5d1e265318c7d7e
mariamonica <monicamicabalo@gmail.com>
2016-07-26 19:22:10 +0800

    Removal

---
+++ app.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const utils = require('./util');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Enter a grade: ',function (answer)  {
+  var grade = (answer);
+  utils.processTheGrade(grade);
+  rl.close();
+});
